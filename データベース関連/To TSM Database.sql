4ec693004d6a5e868335756e323ccbb396e5a323
Mitsutan <petitcom_hondou@icloud.com>
2022-10-27 10:19:39 +0900

    データベース（暫定）
    データベース関連のフォルダを作成

---
+++ データベース関連/To TSM Database.sql
@@ -0,0 +1,64 @@
+CREATE TABLE customers(
+    customers_mail VARCHAR(191) PRIMARY KEY,
+    customers_pass VARCHAR(191) NOT NULL,
+    customers_nick VARCHAR(191) NOT NULL,
+    customers_ln VARCHAR(191) NOT NULL,
+    customers_fn VARCHAR(191) NOT NULL,
+    customers_postcode CHAR(7) NOT NULL,
+    customers_pref VARCHAR(191) NOT NULL,
+    customers_address VARCHAR(191) NOT NULL,
+    customers_subaddress VARCHAR(191) NOT NULL
+);
+CREATE TABLE makers(
+    makers_id INT PRIMARY KEY,
+    makers_name VARCHAR(191) NOT NULL
+);
+CREATE TABLE buys(
+    buys_id INT PRIMARY KEY,
+    buys_date DATE NOT NULL,
+    customers_mail VARCHAR(191) NOT NULL,
+    buys_payment INT NOT NULL,
+    FOREIGN KEY fkey1 (customers_mail) REFERENCES customers(customers_mail)
+);
+CREATE TABLE items(
+    items_id INT PRIMARY KEY,
+    makers_id INT NOT NULL,
+    items_name VARCHAR(191) NOT NULL,
+    items_description VARCHAR(191) NOT NULL,
+    items_price INT NOT NULL,
+    items_url VARCHAR(191) NOT NULL,
+    items_genre VARCHAR(191) NOT NULL,
+    items_material VARCHAR(191) NOT NULL,
+    items_color VARCHAR(191) NOT NULL,
+    FOREIGN KEY fkey1 (makers_id) REFERENCES makers(makers_id)
+);
+CREATE TABLE itemsinfo(
+    items_id INT,
+    itemsinfo_size VARCHAR(191),
+    itemsinfo_stock INT NOT NULL,
+    itemsinfo_width INT,
+    itemsinfo_height INT,
+    itemsinfo_shoulder INT,
+    itemsinfo_length INT,
+    itemsinfo_sleeve INT,
+    itemsinfo_total INT,
+    itemsinfo_waist INT,
+    itemsinfo_hip INT,
+    itemsinfo_pants INT,
+    itemsinfo_aroundsleeve INT,
+    itemsinfo_inseam INT,
+    itemsinfo_rise INT,
+    itemsinfo_thigh INT,
+    PRIMARY KEY(items_id, itemsinfo_size),
+    FOREIGN KEY fkey1 (items_id) REFERENCES items(items_id)
+);
+CREATE TABLE details(
+    buys_id INT,
+    items_id INT,
+    itemsinfo_size VARCHAR(191),
+    details_vol INT NOT NULL,
+    PRIMARY KEY(buys_id, items_id, itemsinfo_size),
+    FOREIGN KEY fkey1 (buys_id) REFERENCES buys(buys_id),
+    FOREIGN KEY fkey2 (items_id) REFERENCES items(items_id),
+    FOREIGN KEY fkey3 (itemsinfo_size) REFERENCES itemsinfo(itemsinfo_size)
+);
