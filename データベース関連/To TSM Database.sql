9730fe9bf8bfb96f76ee4b8d5d98dd6f3e351d7c
Mitsutan <petitcom_hondou@icloud.com>
2022-11-01 09:31:23 +0900

    Update To TSM Database.sql
    auto increment追加
    未解決の不具合について注釈を追加

--- データベース関連/To TSM Database.sql
+++ データベース関連/To TSM Database.sql
@@ -10,18 +10,18 @@ CREATE TABLE customers(
     customers_subaddress VARCHAR(191)
 );
 CREATE TABLE makers(
-    makers_id INT PRIMARY KEY,
+    makers_id INT PRIMARY KEY AUTO_INCREMENT,
     makers_name VARCHAR(191) NOT NULL
 );
 CREATE TABLE buys(
-    buys_id INT PRIMARY KEY,
+    buys_id INT PRIMARY KEY AUTO_INCREMENT,
     buys_date DATE NOT NULL,
     customers_mail VARCHAR(191) NOT NULL,
     buys_payment INT NOT NULL,
     FOREIGN KEY fkey1 (customers_mail) REFERENCES customers(customers_mail)
 );
 CREATE TABLE items(
-    items_id INT PRIMARY KEY,
+    items_id INT PRIMARY KEY AUTO_INCREMENT,
     makers_id INT NOT NULL,
     items_name VARCHAR(191) NOT NULL,
     items_description VARCHAR(191) NOT NULL,
@@ -52,6 +52,7 @@ CREATE TABLE itemsinfo(
     FOREIGN KEY fkey3 (items_id) REFERENCES items(items_id),
     PRIMARY KEY(items_id, itemsinfo_size)
 );
+-- fkey5について注釈：lolipopのphpmyadminだとgui上でfkey5の外部キー制約が設定されませんが、内部的には参照しているようなので無視してください。
 CREATE TABLE details(
     buys_id INT,
     items_id INT,
@@ -59,6 +60,5 @@ CREATE TABLE details(
     details_vol INT NOT NULL,
     FOREIGN KEY fkey4 (buys_id) REFERENCES buys(buys_id),
     FOREIGN KEY fkey5 (items_id, itemsinfo_size) REFERENCES itemsinfo(items_id, itemsinfo_size),
-    -- FOREIGN KEY fkey6 (itemsinfo_size) REFERENCES itemsinfo(itemsinfo_size),
     PRIMARY KEY(buys_id, items_id, itemsinfo_size)
 );
