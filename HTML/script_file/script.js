01ee80d3b97dde5b1bfe59b80725816395aa01ba
Mitsutan <petitcom_hondou@icloud.com>
2022-11-11 22:33:15 +0900

    引っ越しに伴う変更
    urlをぱっと見わかる範囲で修正
    引っ越したことで汎用化できるようになったところを修正

--- HTML/script_file/script.js
+++ HTML/script_file/script.js
@@ -116,3 +116,71 @@ function delFavorite(itemId) {
     // localstorageに保存
     localStorage.setItem('favItems', JSON.stringify(Array.from(favItemsList), undefined, 1));
 }
+
+// 商品カード生成機能（引数：親要素id属性、商品ID、ジャンル、メーカー、商品名、価格、url）
+function createItemCard(parent, itemId, genre, maker, itemName, price, url) {
+    let parentDiv = document.getElementById(parent);
+    const cardDiv = document.createElement('div');
+    const cardForm = document.createElement('form');
+    const cardId = document.createElement('input');
+    const cardBtn = document.createElement('button');
+    // const cardImg = document.createElement('input');
+    const cardImg = document.createElement('img');
+    const cardBody = document.createElement('div');
+    const cardH5 = document.createElement('h5');
+    const cardLikeDiv1 = document.createElement('div');
+    const cardLikeDiv2 = document.createElement('div');
+    const cardInput = document.createElement('input');
+    const cardLabel = document.createElement('label');
+    const cardI = document.createElement('i');
+    const cardName = document.createElement('h6');
+    const cardPrice = document.createElement('h6');
+
+    cardDiv.setAttribute('class', 'card col-sm-3');
+    cardForm.setAttribute('action', 'Shohin_seni.html');
+    cardForm.setAttribute('method', 'get');
+    cardForm.setAttribute('class', 'img_unification card');
+    cardId.setAttribute('type', 'hidden');
+    cardId.setAttribute('name', 'itemId');
+    cardId.setAttribute('value', itemId);
+    cardBtn.setAttribute('type', 'submit');
+    cardBtn.setAttribute('class', 'card-btn');
+    cardImg.setAttribute('class', 'card-img-top img-fluid')
+    // cardImg.setAttribute('type', 'image');
+    cardImg.setAttribute('src', './' + genre + '_photo/' + url);
+    // cardImg.setAttribute('onClick', "location.href='./Shohin_seni.html'");
+    cardBody.setAttribute('class', 'card-body');
+    cardH5.setAttribute('class', 'card-title');
+    cardLikeDiv1.setAttribute('align', 'right');
+    cardLikeDiv2.setAttribute('class', 'example2');
+    cardInput.setAttribute('type', 'checkbox');
+    cardInput.setAttribute('name', 'example2');
+    cardInput.setAttribute('id', itemId);
+    cardInput.setAttribute('onClick', 'checkFavSt(' + itemId + ')');
+    if (new Set(getFavorite()).has(itemId)) {
+        cardInput.setAttribute('checked', 'true');
+    }
+    cardLabel.setAttribute('for', itemId);
+    cardI.setAttribute('class', 'bi bi-heart-fill fs-5');
+    cardName.setAttribute('class', 'clear');
+    cardPrice.setAttribute('class', 'nedan_2');
+
+    cardH5.textContent = maker;
+    cardName.textContent = itemName;
+    cardPrice.textContent = "¥" + price;
+
+    parentDiv.appendChild(cardDiv);
+    cardDiv.appendChild(cardForm);
+    cardForm.appendChild(cardId);
+    cardForm.appendChild(cardBtn);
+    cardBtn.appendChild(cardImg);
+    cardDiv.appendChild(cardBody);
+    cardBody.appendChild(cardH5);
+    cardBody.appendChild(cardLikeDiv1);
+    cardLikeDiv1.appendChild(cardLikeDiv2);
+    cardLikeDiv2.appendChild(cardInput);
+    cardLikeDiv2.appendChild(cardLabel);
+    cardLabel.appendChild(cardI);
+    cardBody.appendChild(cardName);
+    cardBody.appendChild(cardPrice);
+}
