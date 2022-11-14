cb9fd4e724e3be2682ea2ee5bd8e6483fdc18368
anusFuck <2101026@s.asojuku.ac.jp>
2022-11-14 23:02:22 +0900

    Update newFunction.js

--- HTML/script_file/newFunction.js
+++ HTML/script_file/newFunction.js
@@ -1,58 +1,29 @@
-// function HaishokuColor(){
-//     if(document.getElementById('color1').checked){
-//         const color = document.getElementById("select-color");
-//         color.innerHTML="オリーブ";
-//         document.getElementById("img-color-1").src = "./sweat_photo/ahe.jpg";
-//         document.getElementById("img-color-2").src = "./sweat_photo/ahe.jpg";
-//         document.getElementById("img-color-3").src = "./sweat_photo/ahe.jpg";
-//         document.getElementById("img-color-4").src = "./sweat_photo/ahe.jpg";
-//         document.getElementById('kakusu').style.display = 'inline';
-//     }else if(document.getElementById('color2').checked){
-//         const color = document.getElementById("select-color");
-//         color.innerHTML="黒";
-//         document.getElementById("img-color-1").src = "./pants_photo/fAQfYvy8ki1D-768.jpg";
-//         document.getElementById("img-color-2").src = "./pants_photo/fAQfYvy8ki1D-768.jpg";
-//         document.getElementById("img-color-3").src = "./pants_photo/fAQfYvy8ki1D-768.jpg";
-//         document.getElementById("img-color-4").src = "./pants_photo/fAQfYvy8ki1D-768.jpg";
-//         document.getElementById('kakusu').style.display = 'inline';
-//     }else{
-//         const color = document.getElementById("select-color");
-//         color.innerHTML="青";
-//         document.getElementById("img-color-1").src = "./outer_photo/download.jpg";
-//         document.getElementById("img-color-2").src = "./outer_photo/download.jpg";
-//         document.getElementById("img-color-3").src = "./outer_photo/download.jpg";
-//         document.getElementById('kakusu').style.display = 'inline';
-//         document.getElementById("img-color-4").src = "./outer_photo/download.jpg";
-        
-//     }
-// }
-
 function HaishokuColor1(){
   const color = document.getElementById("select-color");
   color.innerHTML="オリーブ";
-    document.getElementById("img-color-1").src = "./sweat_photo/ahe.jpg";
-    document.getElementById("img-color-2").src = "./sweat_photo/ahe.jpg";
-    document.getElementById("img-color-3").src = "./sweat_photo/ahe.jpg";
-    document.getElementById("img-color-4").src = "./sweat_photo/ahe.jpg";
+    document.getElementById("img-color-1").src = "./pants_photo/olive-pants.jpg";
+    document.getElementById("img-color-2").src = "./pants_photo/olive2-pants.jpg";
+    document.getElementById("img-color-3").src = "./pants_photo/olive3-pants.jpg";
+    document.getElementById("img-color-4").src = "./pants_photo/olive4-pants.jpg";
     document.getElementById('kakusu').style.display = 'inline';
 }
 
 function HaishokuColor2(){
     const color = document.getElementById("select-color");
     color.innerHTML="黒";
-    document.getElementById("img-color-1").src = "./pants_photo/fAQfYvy8ki1D-768.jpg";
+    document.getElementById("img-color-1").src = "./sweat_photo/dark-sweat.jpg";
     document.getElementById("img-color-2").src = "./pants_photo/fAQfYvy8ki1D-768.jpg";
-    document.getElementById("img-color-3").src = "./pants_photo/fAQfYvy8ki1D-768.jpg";
-    document.getElementById("img-color-4").src = "./pants_photo/fAQfYvy8ki1D-768.jpg";
+    document.getElementById("img-color-3").src = "./shoes_photo/dark1-shoes.jpg";
+    document.getElementById("img-color-4").src = "./pants_photo/dark-pants.jpg";
     document.getElementById('kakusu').style.display = 'inline';
 }
 
 function HaishokuColor3(){
     const color = document.getElementById("select-color");
     color.innerHTML="青";
-    document.getElementById("img-color-1").src = "./outer_photo/download.jpg";
-    document.getElementById("img-color-2").src = "./outer_photo/download.jpg";
-    document.getElementById("img-color-3").src = "./outer_photo/download.jpg";
-    document.getElementById("img-color-4").src = "./outer_photo/download.jpg";
+    document.getElementById("img-color-1").src = "./pants_photo/bule-pants.jpg";
+    document.getElementById("img-color-2").src = "./shoes_photo/bule-shoes.jpg";
+    document.getElementById("img-color-3").src = "./pants_photo/bule2-pants.jpg";
+    document.getElementById("img-color-4").src = "./shoes_photo/bule2-shoes.jpg";
     document.getElementById('kakusu').style.display = 'inline';
 }
